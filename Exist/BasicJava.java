5fb3249e738b656ced6370886300221812b35c16
Giusippi Maria II Apa <130898831+kyokosawada@users.noreply.github.com>
2025-08-01 19:43:01 +0800

    Add files via upload

--- Exist/BasicJava.java
+++ Exist/BasicJava.java
@@ -7,8 +7,9 @@ public class BasicJava {
 	public static void main(String [] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-        TableGenerator table = new TableGenerator();
-      
+        Table table = new Table();
+      	
+
         table.startTableGeneration();
         boolean exit = false;
 
@@ -19,28 +20,24 @@ public class BasicJava {
         	System.out.println("[ 3 ] - Print");
         	System.out.println("[ 4 ] - Reset");
         	System.out.println("[ x ] - Exit");
-        	System.out.println("Choose an action: ");
+        	System.out.print("Choose an action: ");
 
         	String action = scanner.nextLine();
 
         	switch (action){
+        		case "1" -> table.searchValue();
         		case "2" -> table.editValue();
         		case "3" -> table.printTable();
         		case "4" -> table.resetTable();
         		case "x" -> exit = true; 
-        		default -> System.out.println("wla p");			
+        		default -> System.out.println("Invalid action");			
 
         	}
-
-
-        }
-
-   
-
+        }  
 	}
 }
 
-public class TableGenerator {
+public class Table {
 	// DECLARE string array to hold the locations of the ASCII strings
 	private String [][] table;
 
@@ -50,6 +47,49 @@ public class TableGenerator {
 	// DECLARE string to hold dimension of the Table
 	private int row;
 	private int column;
+
+	// DECLARE the constants for the ascii
+	private static final int ASCII_STRING_LENGTH = 3;
+    private static final int ASCII_MIN = 33;
+    private static final int ASCII_RANGE = 94;
+    private static final int MAX_DIMENSION = 100;
+
+	public boolean validateDimensions(String dimensions){
+
+	    if (Pattern.matches("\\d+x\\d+", dimensions)) {
+	        String[] parts = dimensions.split("x");
+
+	        try{
+		        int inputRow = Integer.parseInt(parts[0]);
+		        int inputColumn = Integer.parseInt(parts[1]);
+		        
+		        return inputRow > 0 && inputColumn > 0 && inputRow <= MAX_DIMENSION && inputColumn <= MAX_DIMENSION;
+	        }
+	        catch(NumberFormatException e){
+	        	return false;
+	        }
+	    }
+	    return false;
+	}
+
+	public boolean validateTableIndex(String tableIndex){
+	    if(Pattern.matches("\\d+,\\d+", tableIndex)){
+	    	String[] parts = tableIndex.split(",");
+	    	try {
+		        Integer.parseInt(parts[0]); 
+		        Integer.parseInt(parts[1]);  
+
+		        return true;
+		    }
+	    	catch(NumberFormatException e) {
+	        	return false;  
+	    	}
+	    }
+	    else{
+	    	return false;
+	    }    
+	}
+
 
 	// DECLARE a method to set dimension of the table
 	public void setTableDimension(String dimensions) {
@@ -68,22 +108,103 @@ public class TableGenerator {
 			for(int j = 0; j < column; j++){
 				generatedAscii = "";
 				// DECLARE for loop 3 times to put random generated ascii to declared string
-				for(int k = 0; k < 3; k++){
+				for(int k = 0; k < ASCII_STRING_LENGTH; k++){
 					// Ascii generator
-					int randomAscii = (int)(Math.random() * 95) + 33;
+					int randomAscii = (int)(Math.random() * ASCII_RANGE) + ASCII_MIN;
 					char randomChar = (char) randomAscii;
 					generatedAscii += randomChar;
 				}
 				//Set string to the location of string array
 				table[i][j] = generatedAscii;
 			}
-			
 		} 
-
 	}
 		
+	public void startTableGeneration(){
+   		Scanner scanner = new Scanner(System.in);
 
+	    while(true) {
+	        System.out.print("Input table dimensions (ROWxCOLUMN): ");
+	        String dimensions = scanner.nextLine();
+	        
+	        if(validateDimensions(dimensions)) {
+	            setTableDimension(dimensions);
+	            generateTable();
+	            break;
+	        } else {
+	            System.out.println("Enter proper format (ROWxCOLUMN)");
+	        }
+	    }
+	}
+ 
 	//DECLARE a method to print the strings array from the array
+	public void searchValue(){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter value to search: ");
+		String valueSearched = scanner.nextLine();
+	 
+	    if(valueSearched.isEmpty()){
+	        System.out.println("Search value can't be empty");
+	        return;
+	    }
+
+	    boolean found = false;
+    
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < column; j++){
+				int occurrences = 0;
+				int index = 0;
+
+				while((index = table[i][j].indexOf(valueSearched, index)) != -1) {
+                	occurrences++;
+                	index++;
+           	 	}
+
+           	 	if(occurrences > 0){
+           	 		found = true;
+           	 		System.out.println(occurrences + " Occurence/s at " + "( " + i + "," + j + " )");
+           	 	}
+			}
+		}
+		if(!found){
+			System.out.println("No occurence/s found in the table");
+		}
+	}	
+
+	public void editValue(){
+		int rowToEdit;
+		int columnToEdit;
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input table index (ROW,COLUMN): ");
+		String tableIndex = scanner.nextLine();  
+              
+        if(validateTableIndex(tableIndex)){
+
+        	String[] index = tableIndex.split(",");
+
+        	
+        	rowToEdit = Integer.parseInt(index[0]);
+	        columnToEdit = Integer.parseInt(index[1]);
+
+			if(rowToEdit < row){
+				if(columnToEdit < column){
+					System.out.print("Enter new value for { " + table[rowToEdit][columnToEdit] + " }: ");
+					String newValue = scanner.nextLine();  
+					table[rowToEdit][columnToEdit] = newValue;
+				}
+				else
+					System.out.println("Enter proper column index");
+			}
+			else
+				System.out.println("Enter proper row index");
+        }
+        else{
+        	System.out.println("Enter proper format (ROW,COLUMN) or number too large");
+        }
+	}
+
 	public void printTable(){		
 		for(int i = 0; i < row; i++){
 			for(int j = 0; j < column; j++)
@@ -93,57 +214,20 @@ public class TableGenerator {
 	}
 
 	public void resetTable(){
-		startTableGeneration();
-
-	}
-
-	public void startTableGeneration(){
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Input table dimensions: ");
-        String dimensions = scanner.nextLine();        
-        System.out.println("You entered: " + dimensions);
+		System.out.print("Input table dimensions (ROWxCOLUMN): ");
+        String dimensions = scanner.nextLine(); 
 
-        if(validateDimensions(dimensions)){
+		if(validateDimensions(dimensions)){
         	setTableDimension(dimensions);
         	generateTable();
         }
         else{
-        	startTableGeneration();
+       	    System.out.println("Enter proper format (ROWxCOLUMN)");
         }
 	}
-	
-	public boolean validateDimensions(String dimensions){
-		return Pattern.matches("\\d+x\\d+", dimensions);
-	}
-	public boolean validateTableIndex(String tableIndex){
-		if(Pattern.matches("\\d+,\\d+", tableIndex)){
-			String[] index = tableIndex.split(",");
-			System.out.println(Integer.parseInt(index[0]));
-			if(Integer.parseInt(index[0]) < row )
-				if(Integer.parseInt(index[1]) < column)
-					return true;
-		}
-		return false;
-	}
 
-	public void editValue(){
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Input table index (row,column): ");
-        String tableIndex = scanner.nextLine();        
-  
-        if(validateTableIndex(tableIndex)){
-        	
-        }
-        else{
-        	System.out.println("Invalid index format");
-        }
- 
-
-	}
-
-	
 }	
 
 
